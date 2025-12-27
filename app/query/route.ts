fb9a9ee2834e4d8e18adad895f4444346b50afc9
sameernadir72 <sameernadir72@gmail.com>
2025-12-27 20:33:18 +0500

    Refactor dashboard components and update database connection; remove .env.example

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,38 @@
-// import postgres from 'postgres';
+import postgres from "postgres";
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
+  const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 500;
+  `;
 
-// 	return data;
-// }
+  return data;
+}
+async function postInvoice() {
+  const data = await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES ('3958dc9e-742f-4377-85e9-fec4b6a6442a', 500, 'paid', '2023-01-01');
+  `;
+
+  return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
+
+export async function POST() {
+  try {
+    return Response.json(await postInvoice());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
